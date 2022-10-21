2a349ce6729433f48378e2d465935d40d899f380
Matias Sio <siomatias20@gmail.com>
2022-10-21 20:14:46 -0300

    Pagina de doctores añadida

---
+++ components/BusquedaDoctores/BusquedaDoctores.jsx
@@ -0,0 +1,33 @@
+
+function BusquedaDoctores() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center mt-10">
+        <div className="flex w-full justify-center items-center">
+        <input
+          className="bg-gray-200 w-2/4 h-10 border rounded-l-xl py-2 px-6 outline-none text-sm font-light"
+          type="text"
+          placeholder="Búsqueda por nombre de doctor..."
+      />
+                <button
+            type="submit"
+            className="bg-button text-white py-2 px-8 rounded-r-xl hover:bg-active"
+          >
+            Buscar
+          </button>
+        </div>
+
+      <div className="my-6">
+      <p>No se encontró un doctor...</p>
+      </div>
+      
+      <div>
+        
+      </div>
+      
+      </div>
+    </>
+  )
+}
+
+export default BusquedaDoctores
---
+++ pages/doctores/index.jsx
@@ -0,0 +1,12 @@
+import BusquedaDoctores from "../../components/BusquedaDoctores/BusquedaDoctores"
+
+function DoctoresPage() {
+  return (
+    <>
+      <p className="font-bold text-2xl">Doctores</p>
+      <BusquedaDoctores />
+    </>
+  )
+}
+
+export default DoctoresPage
