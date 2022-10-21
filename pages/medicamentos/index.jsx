c1f299f7306525efbec48802a21bbb03001a5894
Matias Sio <siomatias20@gmail.com>
2022-10-21 20:21:23 -0300

    Pagina de medicamentos añadida

---
+++ pages/medicamentos/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BusquedaMedicamentos from './../../components/BusquedaMedicamentos/BusquedaMedicamentos';
+
+function MedicamentosPage() {
+  return (
+    <>
+      <p className="font-bold text-2xl">Medicamentos</p>
+      <BusquedaMedicamentos />
+    </>
+  )
+}
+
+export default MedicamentosPage
