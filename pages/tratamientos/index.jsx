362c61b1dba5dfc746e1d966c066edc646a88689
Matias Sio <siomatias20@gmail.com>
2022-10-21 20:24:05 -0300

    Pagina de tratamientos de pacientes añadida

---
+++ pages/tratamientos/index.jsx
@@ -0,0 +1,13 @@
+
+import BusquedaTratamientos from './../../components/BusquedaTratamientos/BusquedaTratamientos';
+
+function TratamientosPage() {
+  return (
+    <>
+      <p className="font-bold text-2xl">Tratamientos de pacientes</p>
+      <BusquedaTratamientos />
+    </>
+  )
+}
+
+export default TratamientosPage
